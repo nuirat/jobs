795563fa4f6ef224e4a60311db344bd7113670af
MohammadYahya <mohamadyahya979@gmail.com>
2022-08-09 11:41:48 -0700

    css

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,36 @@
-const dataModel = new DataModel()
-const renderer = new Renderer()
+const dataModel = new DataModel();
+const renderer = new Renderer();
+
+const jopsData = function () {
+  let city = ["Nablus", "jenin", "Ramallah"];
+
+  let JobType = [
+    "Education and Training.",
+    "Government and Public Administration",
+    "Health Science.",
+    "   Information Technology",
+  ];
+  let years = [
+    "1 year",
+    "2 years",
+    "3 years",
+    "4 years",
+    "5 years",
+    " +5 years",
+  ];
+  const getCitys = () => {
+    return city;
+  };
+  const getYears = () => {
+    return years;
+  };
+  const getJob = () => {
+    return JobType;
+  };
+
+  return {
+    getCitys: getCitys,
+    getYears: getYears,
+    getJob: getJob,
+  };
+};
