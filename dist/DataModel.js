f950a1dd36e8ce5ace67a695de3c7c32365d67a7
nuirat <an13cr7@gmail.com>
2022-08-09 15:03:03 +0300

    get jobs

--- dist/DataModel.js
+++ dist/DataModel.js
@@ -1,6 +1,9 @@
 class DataModel {
   constructor() {
     this.jobData = [];
+    this.city=[] ;
+    this.years=[];
+    this.type=[] ;
   }
   async getJobs() {
     let result = await $.ajax({
@@ -10,4 +13,16 @@ class DataModel {
     this.jobData = result;
     return this.jobData;
   }
+
+  async getCitys() {
+    let result = await $.ajax({
+      method: "GET",
+      url: "/init",
+    });
+  this.city=result.map(job =>job.city)
+  this.type=result.map(job=>job.jobType)
+  this.years=result.map(job=>job.experinceYears)
+  }
+
+ 
 }
