6e50140e3a52d6a99fb13598a3c98bced6623873
MohammadYahya <mohamadyahya979@gmail.com>
2022-08-09 12:45:09 -0700

    mohammad

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const Jobs = require("../models/Job");
-
+router.get("/allJobs", (request, response) => {
+  Jobs.find({}).exec(function (error, dataBase) {
+    response.send(dataBase);
+  });
+});
 router.get("/avaliavbleJobs", (request, response) => {
   const requestObject = {
     city: request.body.city,
     jobType: request.body.jobType,
     experinceYears: request.body.experinceYears,
   };
+
   Jobs.find({
     city: requestObject.city,
     jobType: requestObject.jobType,
